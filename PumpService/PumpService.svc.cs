7deeb1e4a35b93561ce8f62a3692ae9f7a6c02c1
Sergans <gan7er@yandex.ru>
2022-08-17 10:15:38 +0300

    finish service

--- PumpService/PumpService.svc.cs
+++ PumpService/PumpService.svc.cs
@@ -11,21 +11,39 @@ namespace PumpService
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
     // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
     // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
-    public class Service1 : IPumpService
+    public class PumpService : IPumpService
     {
+        private readonly IScriptService _scriptService;
+        private readonly IStatisticsService _statisticsService;
+        private readonly ISettingsService _serviceSettings;
+
+        public PumpService()
+        {
+            _statisticsService = new StatisticsService();
+            _serviceSettings = new SettingsService();
+            _scriptService = new ScriptService(_serviceSettings, _statisticsService, Callback);
+        }
         public void CompileScript()
         {
-            throw new NotImplementedException();
+            _scriptService.Compile();
         }
 
         public void RunScript()
         {
-            throw new NotImplementedException();
+            _scriptService.Run();
         }
 
         public void UpdateAndCompileScript(string fileName)
         {
-            throw new NotImplementedException();
+            _serviceSettings.FileName = fileName;
+            _scriptService.Compile();
+        }
+        IPumpServiceCallback Callback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<IPumpServiceCallback>();
+            }
         }
     }
 }
